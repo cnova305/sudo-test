d961512913362f2dd1ce1d794d29c2d9217e4a61
Christopher Juwao <njuwao@icloud.com>
2024-10-03 00:14:54 +0200

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export const ACCEPTABLE_FILES = {
+  "application/msword": [".doc", ".docx"],
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document": [
+    ".docx",
+  ],
+  "image/png": [".png"],
+  "image/jpeg": [".jpg", ".jpeg"],
+  "application/pdf": [".pdf"],
+};
+export const MAX_FILE_SIZE = 5242880;
